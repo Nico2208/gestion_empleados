1089312c71bc66ec9922187c6dd94c6b5f5837e9
Nicolas Sicolo <sicolo2208@gmail.com>
2022-10-09 19:06:26 -0300

    Modifico password localhost para notebook

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ mysql = MySQL()
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'sistema'
 
 mysql.init_app(app)
